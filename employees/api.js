89012184402074159da32125b24fc037de629392
14nerui <14neruin@gmail.com>
2019-03-10 01:10:59 +0500

    Add api

--- employees/api.js
+++ employees/api.js
@@ -23,5 +23,26 @@ app.get("/api/order/:id", (req,res) => {
 	access()
 })
 
+app.get("/api/users", (req,res) => {
+	let search = (req.query.search)?req.query.search:''
+
+	let access = req.session.token == "123456"?()=>{
+		res.send({"response":[]})
+	}:()=>{
+		res.send({"error":"no access"})
+	}
+	access()
+})
+
+app.get("/api/user/:id", (req,res) => {
+	let id = req.params.id
+
+	let access = req.session.token == "123456"?()=>{
+		res.send({"response":[]})
+	}:()=>{
+		res.send({"error":"no access"})
+	}
+	access()
+})
 
 }
