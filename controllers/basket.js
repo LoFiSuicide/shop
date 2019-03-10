e2d20c898ebe2e8a02d4403501e3d870c5bbca38
14nerui <14neruin@gmail.com>
2019-03-10 13:49:08 +0500

    Edit Controller Basket

--- controllers/basket.js
+++ controllers/basket.js
@@ -16,3 +16,45 @@ exports.price = (req,res) => {
 		res.send({price:result})
 	})
 }
+
+exports.add = (req,res) => {
+	let id = req.params.id
+	let search = 0
+	for(product of req.session.basket){
+		if(product.id == id){
+			product.col++
+			search = 1
+		}
+	}
+	if(search == 0)
+		req.session.basket[req.session.basket.length] = {id:id, col:1}
+
+	req.session.save()
+	res.send({status:true})
+}
+
+exports.edit = (req,res) => {
+	let id = req.query.id
+	let col = req.query.col
+	for(i in req.session.basket){
+		if(req.session.basket[i].id == id){
+			if(Number(req.session.basket[i].col) + Number(col) <= 0)
+				req.session.basket.splice(i, 1)
+			else
+				req.session.basket[i].col = Number(req.session.basket[i].col) + Number(col)
+		}
+	}
+	req.session.save()
+	res.send({status:true})
+}
+
+exports.delete = (req,res) => {
+	let id = req.params.id
+	let search = 0
+	for(i in req.session.basket){
+		if(req.session.basket[i].id == id)
+			req.session.basket.splice(i, 1)
+	}
+	req.session.save()
+	res.send({status:true})
+}
--- shop/api.js
+++ shop/api.js
@@ -3,7 +3,6 @@ module.exports = (app) => {
 let productsContoller = require('../controllers/products')
 let basketController = require('../controllers/basket')
 
-
 //Конфиг
 app.get("/api", (req,res) => res.send({ ver:'1.0', lang:"ru, en"}))
 
@@ -20,54 +19,13 @@ app.get("/api/categories/:id", productsContoller.category)
 
 //Корзина
 app.get("/api/basket", basketController.get)
-
-let db = require('../db.js')
 //Расчет итоговой стоимости
 app.get("/api/price", basketController.price)
-
-//Уменьшаем/увеличиваем количество товара
-app.get("/api/editbasket", (req,res) => {
-	let id = req.query.id
-	let col = req.query.col
-	for(i in req.session.basket){
-		if(req.session.basket[i].id == id){
-			if(Number(req.session.basket[i].col) + Number(col) <= 0)
-				req.session.basket.splice(i, 1)
-			else
-				req.session.basket[i].col = Number(req.session.basket[i].col) + Number(col)
-		}
-	}
-	req.session.save()
-	res.send({status:true})
-})
-
 //Добавление продукта в корзину
-app.get("/api/addtobasket:id", (req,res) => {
-	let id = req.params.id
-	let search = 0
-	for(product of req.session.basket){
-		if(product.id == id){
-			product.col++
-			search = 1
-		}
-	}
-	if(search == 0)
-		req.session.basket[req.session.basket.length] = {id:id, col:1}
-
-	req.session.save()
-	res.send({status:true})
-})
-
+app.get("/api/addtobasket:id", basketController.add)
+//Уменьшаем/увеличиваем количество товара
+app.get("/api/editbasket", basketController.edit)
 //Удаление продукта из корзины
-app.get("/api/deletetobasket:id", (req,res) => {
-	let id = req.params.id
-	let search = 0
-	for(i in req.session.basket){
-		if(req.session.basket[i].id == id)
-			req.session.basket.splice(i, 1)
-	}
-	req.session.save()
-	res.send({status:true})
-})
+app.get("/api/deletetobasket:id", basketController.delete)
 
 }
