0a8e6c020c7045c4e754b9f0819bdfa058a3c45c
14nerui <14neruin@gmail.com>
2019-03-10 11:51:50 +0500

    edit struct proj

---
+++ db.js
@@ -0,0 +1,32 @@
+let mysql = require('mysql'),
+	fs = require('fs')
+
+let dbConnect = JSON.parse(fs.readFileSync('dbconnect.json', 'utf8'))
+
+let state = {
+	db:null
+}
+
+exports.connect = (done) => {
+	if(state.db)
+		return done()
+	
+	let connection = mysql.createConnection(dbConnect)
+
+	connection.connect((err) => {
+		if (err) {
+			console.error('error connecting: ' + err.stack);
+			return done(err);
+		}
+		state.db = connection
+		return done()
+	})
+}
+
+exports.get = () => {
+	return state.db
+}
+
+exports.end = () => {
+	state.db = null
+}
--- shop/api.js
+++ shop/api.js
@@ -1,5 +1,6 @@
 module.exports = (app, mysql, dbConnect) => {
 
+let db = require('../db.js')
 //Конфиг
 app.get("/api", (req,res) => {
 	res.send({ ver:'1.0', lang:"ru, en"})
@@ -10,92 +11,25 @@ app.get("/api/basket", (req,res) => {
 	res.send(req.session.basket)
 })
 
-//Расчет итоговой стоимости
-app.get("/api/price", (req,res) => {
-	let ids = ""
-	for(product of req.session.basket){
-		ids += `${product.id},`
-	}
-	let connection = mysql.createConnection(dbConnect)
-	connection.query(`SELECT * FROM products WHERE id_product IN (${ids}0)`, (err, result) => {
-		if(err != null){
-			console.log(`ERROR getCategory: ${err}`)
-			res.send({error: true})
-		}
-		else{
-			let sum = 0
-			for(product of req.session.basket){
-				for(prod_info of result){
-					if(prod_info.id_product == product.id){
-						sum+=prod_info.price*product.col
-						break
-					}
-				}
-			}
-			res.send({price:sum})
-		}
-	})
-	connection.end()
-})
-
-//Уменьшаем/увеличиваем количество товара
-app.get("/api/editbasket", (req,res) => {
-	let id = req.query.id
-	let col = req.query.col
-	for(i in req.session.basket){
-		if(req.session.basket[i].id == id){
-			if(Number(req.session.basket[i].col) + Number(col) <= 0)
-				req.session.basket.splice(i, 1)
-			else
-				req.session.basket[i].col = Number(req.session.basket[i].col) + Number(col)
-		}
-	}
-	req.session.save()
-	res.send({status:true})
-})
-
-//Добавление продукта в корзину
-app.get("/api/addtobasket:id", (req,res) => {
-	let id = req.params.id
-	let search = 0
-	for(product of req.session.basket){
-		if(product.id == id){
-			product.col++
-			search = 1
-		}
-	}
-	if(search == 0)
-		req.session.basket[req.session.basket.length] = {id:id, col:1}
-
-	req.session.save()
-	res.send({status:true})
-})
-
-//Удаление продукта из корзины
-app.get("/api/deletetobasket:id", (req,res) => {
-	let id = req.params.id
-	let search = 0
-	for(i in req.session.basket){
-		if(req.session.basket[i].id == id)
-			req.session.basket.splice(i, 1)
-	}
-	req.session.save()
-	res.send({status:true})
-})
-
 //Данные о товаре
 app.get("/api/product/:id", (req,res) => {
 	let id = req.params.id
-	let connection = mysql.createConnection(dbConnect)
-	connection.query(`SELECT * FROM products WHERE id_product = ${id}`, (err, result) => {
-		if(err != null){
-			console.log(`ERROR getCategory: ${err}`)
-			res.send({error: true})
-		}
-		else
-			res.send({response:result})
+
+	db.connect((err)=>{
+		if(err)
+			return console.log(err)
+
+		let q = `SELECT * FROM products WHERE id_product = ${id}`
+		db.get().query(q, (err, result) => {
+			if(err != null){
+				console.log(`ERROR getCategory: ${err}`)
+				res.send({error: true})
+			}
+			else
+				res.send({response:result})
+		})
+		db.end()
 	})
-	connection.end()
 })
 
 //Товары определенной категории
@@ -169,4 +103,83 @@ app.get("/api/categories/:id", function(req,res){
 	})
 	connection.end()
 })
+
+//Расчет итоговой стоимости
+app.get("/api/price", (req,res) => {
+	let ids = ""
+	for(product of req.session.basket){
+		ids += `${product.id},`
+	}
+	db.connect((err)=>{
+		if(err)
+			return console.log(err)
+
+		let q = `SELECT * FROM products WHERE id_product IN (${ids}0)`
+		db.get().query(q, (err, result) => {
+			if(err != null){
+				console.log(`ERROR getCategory: ${err}`)
+				res.send({error: true})
+			}
+			else{
+				let sum = 0
+				for(product of req.session.basket){
+					for(prod_info of result){
+						if(prod_info.id_product == product.id){
+							sum+=prod_info.price*product.col
+							break
+						}
+					}
+				}
+				res.send({price:sum})
+			}
+		})
+		db.end()
+	})
+})
+
+//Уменьшаем/увеличиваем количество товара
+app.get("/api/editbasket", (req,res) => {
+	let id = req.query.id
+	let col = req.query.col
+	for(i in req.session.basket){
+		if(req.session.basket[i].id == id){
+			if(Number(req.session.basket[i].col) + Number(col) <= 0)
+				req.session.basket.splice(i, 1)
+			else
+				req.session.basket[i].col = Number(req.session.basket[i].col) + Number(col)
+		}
+	}
+	req.session.save()
+	res.send({status:true})
+})
+
+//Добавление продукта в корзину
+app.get("/api/addtobasket:id", (req,res) => {
+	let id = req.params.id
+	let search = 0
+	for(product of req.session.basket){
+		if(product.id == id){
+			product.col++
+			search = 1
+		}
+	}
+	if(search == 0)
+		req.session.basket[req.session.basket.length] = {id:id, col:1}
+
+	req.session.save()
+	res.send({status:true})
+})
+
+//Удаление продукта из корзины
+app.get("/api/deletetobasket:id", (req,res) => {
+	let id = req.params.id
+	let search = 0
+	for(i in req.session.basket){
+		if(req.session.basket[i].id == id)
+			req.session.basket.splice(i, 1)
+	}
+	req.session.save()
+	res.send({status:true})
+})
+
 }
